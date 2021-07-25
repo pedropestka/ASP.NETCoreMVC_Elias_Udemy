e0d74f3914cf8ed851c25139dce23d2b8bfc818b
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-07-25 15:32:10 -0300

    Select no campo nível - aula 34

--- Site01/Controllers/PalavraController.cs
+++ Site01/Controllers/PalavraController.cs
@@ -2,6 +2,7 @@
 using Site01.Database;
 using Site01.Library.Filters;
 using Site01.Models;
+using System.Collections.Generic;
 using System.Linq;
 using X.PagedList;
 
@@ -10,10 +11,16 @@ namespace Site01.Controllers
     [Login]
     public class PalavraController : Controller
     {
+        List<Nivel> niveis = new List<Nivel>();
+
         private DatabaseContext _db;
         public PalavraController(DatabaseContext db)
         {
             _db = db;
+            
+            niveis.Add(new Nivel() { Id = 1, Nome = "Fácil" });
+            niveis.Add(new Nivel() { Id = 2, Nome = "Médio" });
+            niveis.Add(new Nivel() { Id = 3, Nome = "Difícil" });
         }
 
         // Listar todas as palavras do banco de dados
@@ -36,12 +43,16 @@ namespace Site01.Controllers
         [HttpGet] // Visualizar o formulario
         public IActionResult Cadastrar()
         {
+            ViewBag.Nivel = niveis;
+
             return View(new Palavra());
         }
 
         [HttpPost] // Cadastrar a palavra
         public IActionResult Cadastrar([FromForm] Palavra palavra)
         {
+            ViewBag.Nivel = niveis;
+
             if (ModelState.IsValid)
             {
                 _db.Palavras.Add(palavra); // passo a palavra que veio do fromulario
@@ -49,7 +60,6 @@ namespace Site01.Controllers
 
                 // TEMPDATA
                 TempData["Mensagem"] = "A palavra '" + palavra.Nome + "' foi cadastrada com sucesso!";
-
 
                 return RedirectToAction("Index"); // para ver o registro sendo consultado
             }
@@ -60,6 +70,8 @@ namespace Site01.Controllers
         [HttpGet]
         public IActionResult Atualizar(int Id)
         {
+            ViewBag.Nivel = niveis;
+
             Palavra palavra = _db.Palavras.Find(Id);
             
             return View("Cadastrar", palavra);
@@ -68,6 +80,8 @@ namespace Site01.Controllers
         [HttpPost]
         public IActionResult Atualizar([FromForm] Palavra palavra)
         {
+            ViewBag.Nivel = niveis;
+
             if (ModelState.IsValid)
             {
                 _db.Palavras.Update(palavra);
---
+++ Site01/Models/Nivel.cs
@@ -0,0 +1,8 @@
+﻿namespace Site01.Models
+{
+    public class Nivel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
